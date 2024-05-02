ef5986a0a670a5f2ff77b91ac827cf73494ec544
Louis Mario <louism4rio@gmail.com>
2024-05-02 15:17:04 +0700

    DONE

--- 0706012210005-LouisMarioWijaya-AFL3/_706012210005_LouisMarioWijaya_AFL3App.swift
+++ 0706012210005-LouisMarioWijaya-AFL3/_706012210005_LouisMarioWijaya_AFL3App.swift
@@ -7,18 +7,33 @@
 
 import SwiftUI
 
+
 @main
-struct _706012210005_LouisMarioWijaya_AFL3App: App {
+struct LandmarksApp: App {
     @State private var modelData = ModelData()
+    
     
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(modelData)
         }
+#if !os(watchOS)
+        .commands {
+            LandmarkCommands()
+        }
+#endif
+        
         
 #if os(watchOS)
         WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
+#endif
+        
+        
+#if os(macOS)
+        Settings {
+            LandmarkSettings()
+        }
 #endif
     }
 }
